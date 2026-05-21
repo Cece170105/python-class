ccc98cdae55b6b221738e7055dee5a16a3d5d35e
Cece170105 <MARINA.ROLON52@login.cuny.edu>
2026-05-21 07:33:24 -0400

    Add files via upload

---
+++ examples.py
@@ -0,0 +1,22 @@
+'''instantiating a class means creating an object from a class.
+
+Example:
+
+class Student:
+    pass
+
+# Creating an object
+student1 = Student()
+'''
+#1
+# Create a class named ETStudent
+'''
+class ETStudent:
+
+    # Constructor method
+    def _init_(self, name, student_id):
+
+        # Instance variables
+        self.name = name
+        self.student_id = student_id
+        '''
---
+++ examples2.py
@@ -0,0 +1,45 @@
+'''2
+# Create a class named ETStudent
+class ETStudent:
+
+    # Constructor method
+    def _init_(self, name, student_id):
+
+        # Instance variables
+        self.name = name
+        self.student_id = student_id
+
+    # Method to display student information
+    def Student_display(self):
+
+        print("Name:", self.name)
+        print("Id: QCC-ET-" + str(self.student_id))
+        '''
+''' 3
+# Create a class named ETStudent
+class ETStudent:
+
+    # Constructor method
+    def _init_(self, name, student_id):
+
+        # Instance variables
+        self.name = name
+        self.student_id = student_id
+
+    # Method to display student information
+    def Student_display(self):
+
+        print("Name:", self.name)
+        print("Id: QCC-ET-" + str(self.student_id))
+
+
+# Instantiate object
+student1 = ETStudent("Celeste", 987654)
+
+# Print values
+student1.Student_display()
+
+# 4
+Aquí solo tienes que subir el archivo .py completo con todo junto.
+Literalmente el código de arriba guardado como algo tipo:
+ETStudent.py'''
